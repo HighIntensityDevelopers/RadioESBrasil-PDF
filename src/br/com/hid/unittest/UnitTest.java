cc16858bf0f7cdf66bb97d29a15fa749a22c481a
deividfae <deividfae@gmail.com>
2014-03-07 15:28:22 -0300

    Projeto inicial do sistema de download das revistas do ESBrasil

---
+++ src/br/com/hid/unittest/UnitTest.java
@@ -0,0 +1,20 @@
+package br.com.hid.unittest;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+@ManagedBean
+public class UnitTest {
+
+	@PostConstruct
+	public void init() {
+		System.out.println(" Bean executado! ");
+	}
+
+	public String getMessage() {
+		return "Hello World JSF!";
+	}
+
+}
